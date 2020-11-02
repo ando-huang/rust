a6a67a62231c462c776c58d0b86fa59c8ff35c0e
ando-huang <ahuang09182000@gmail.com>
2020-11-01 23:39:36 -0500

    first attempt at user input

--- adventure_game/src/main.rs
+++ adventure_game/src/main.rs
@@ -8,11 +8,30 @@
  *  ***This is the way***
 **/
 mod character;
-//use std::env;
+use std::io;
 
 fn main(){
 
     println!("Welcome to the Rust Adventure game!");
-    let mut character = character::Character::new("Andrew");
+    let mut player = character::Character::new("Andrew");
+    println!("Is your character's name {}?", character::Character::get_name(&player));
+    //Yes or no check, change if no
+    let input = get_player_input();
     
+     
+}
+
+fn get_player_input() -> bool {
+    let mut input_text = String::new();
+    io::stdin().read_line(&mut input_text).expect("std read failed");
+    let reduced = input_text.trim();
+    match reduced.parse::<bool>(){
+        Ok(i) => {
+            return i;
+        },
+        Err(..) => {
+            println!("Wrong format, enter again.");
+            return get_player_input();
+        },
+    };
 }
